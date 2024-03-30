884891eb2cd35489a5137f3b774e15c1fdd0c722
이원규 <103304455+WKlee0607@users.noreply.github.com>
2024-03-30 11:00:23 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,17 @@
 # settings 
-# Download NYU_LABELD: http://horatio.cs.nyu.edu/mit/silberman/nyu_depth_v2/nyu_depth_v2_labeled.mat
+# dataset download: https://cvg.cit.tum.de/data/datasets/rgbd-dataset/download#freiburg2_pioneer_360
+# cams params: https://cvg.cit.tum.de/data/datasets/rgbd-dataset/file_formats
 
-NYU_LABLED_PATH = "./nyu_depth_v2_labeled.mat"
+RGBD_DATASET_ROOT_PATH = "./datas/rgbd_dataset_freiburg2_pioneer_360/"
+# 순서대로 읽어오면 됨. 각각이 순서대로 매칭됨.
+DEPTH_TXT = RGBD_DATASET_ROOT_PATH + "depth.txt"
+RGB_TXT = RGBD_DATASET_ROOT_PATH + "rgb.txt"
+GT_TXT = RGBD_DATASET_ROOT_PATH + "groundtruth.txt" # pose info
+
+#TUM Dataset camera params
+fx = 520.9  # focal length x
+fy = 521.0  # focal length y
+cx = 325.1  # optical center x
+cy = 249.7  # optical center y
+img_height = 480
+img_width = 640
