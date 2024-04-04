431d1a636934731bca8f10d07d2e6d4519831228
이원규 <103304455+WKlee0607@users.noreply.github.com>
2024-04-04 15:48:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import cv2
-
 from create_ptcld_functions import *
 from settings import *
 from fn import *
+from voxel import *
+
+def main(step, img_dataType):
+    datas, intrinsic = get_datas_multiple_timestep(step, img_dataType)
+    voxels = Recon3Dpcd(datas, intrinsic)
+    voxels.visualize()
 
 
-rgb_path_list = open_txt_file(RGB_TXT) # len: 1225
-depth_path_list = open_txt_file(DEPTH_TXT) # len: 1209
-gt_path_list = open_txt_file(GT_TXT) # len: 21823
+    return 
 
-idx = 0
-rgb_path = get_path(idx ,rgb_path_list, "rgb")
-depth_path = get_path(idx, depth_path_list, "")
 
-intrinsic = o3d.camera.PinholeCameraIntrinsic(img_width, img_height, fx, fy, cx, cy)
-pcd = PCD(RGBD(0, rgb_path, depth_path).run()).run()
+
+step = 40 # 가져올 이미지 및 센서 데이터 수
+img_dataType = 1 # data_type: {0: 기본, 1: TUM, 2:np 입력}
+main(step, img_dataType)
