f359ef86b8ab33149856d8f92fc27603762a3d49
이원규 <103304455+WKlee0607@users.noreply.github.com>
2024-04-04 15:47:50 +0900

    Update create_ptcld_functions.py

--- create_ptcld_functions.py
+++ create_ptcld_functions.py
@@ -3,8 +3,8 @@ import open3d as o3d
 
 class RGBD:
     def __init__(self, data_type = 0, rgb_path = '', depth_path = '', np_rgb = None, np_depth = None): # basic format
-        self.data_type = data_type
-        if data_type == 0:
+        self.data_type = data_type # data_type: {0: 기본, 1: TUM, 2:np 입력}
+        if data_type == 0 or data_type == 1:
             self.color_path = rgb_path
             self.depth_path = depth_path
         else:
@@ -17,7 +17,10 @@ class RGBD:
         try:
             color_raw = o3d.io.read_image(self.color_path) # color_path: *.jpg or *.png
             depth_raw = o3d.io.read_image(self.depth_path) # depth_path: *.jpg or *.png
-            rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_raw, depth_raw) #convert_rgb_to_intensity = False)
+            if self.data_type == 0: # basic
+                rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_raw, depth_raw) #convert_rgb_to_intensity = False)
+            elif self.data_type == 1: # TUM
+                rgbd_image = o3d.geometry.RGBDImage.create_from_tum_format(color_raw, depth_raw, convert_rgb_to_intensity = False)
             self.rgbd_image = rgbd_image
             return self.rgbd_image # o3d.geometry.RGBDImage
         except Exception as e:
@@ -35,7 +38,7 @@ class RGBD:
     
     # run
     def run(self): # convert RGB-D to o3d.RGBDImage 
-        if self.data_type == 0:  
+        if self.data_type in [0,1]:  
             return self.rgbdpath_to_o3drgbd() # input rgbd: file path
         else: 
             return self.nparr_to_o3drgbd() # input rgbd: np_array
@@ -48,19 +51,16 @@ class PCD:
     
     def create_pcd_from_rgbd(self, intrinsic:o3d.camera.PinholeCameraIntrinsic, extrinsic:np.ndarray): 
         # convert rgbd to point cloud
-        
-
         pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
             self.rgbd_image,
             intrinsic,
             extrinsic
         )
-        print(pcd)
         # Flip it, otherwise the pointcloud will be upside down
         pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
         self.pcd = pcd
         return pcd # o3d.cpu.pybind.geometry.PointCloud
-    
+
      # visualize point cloud
     def visualize_pcd(self):
         o3d.visualization.draw_geometries([self.pcd])
@@ -70,10 +70,6 @@ class PCD:
             intrinsic:o3d.camera.PinholeCameraIntrinsic = o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault),
             extrinsic:np.ndarray = np.eye(4, dtype=np.float64)
             ): # convert o3d.RGBDImage to o3d.Pcd & visualization
-        self.create_pcd_from_rgbd(intrinsic, extrinsic)
-        self.visualize_pcd()
-    
-
-
+        return self.create_pcd_from_rgbd(intrinsic, extrinsic)
 
 
